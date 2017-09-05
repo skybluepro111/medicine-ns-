8c437602a8acdf00da74b573c2daf52e47428f6f
skybluepro111 <TopFrontForward@mail.com>
2017-09-05 18:58:57 +0300

    Remove the now ignored directory node_module

--- node_modules/nativescript-fancy-calendar/src/calendar-settings.d.ts
+++
@@ -1,9 +0,0 @@
-import { DISPLAY_MODE, SELECTION_MODE, SCROLL_ORIENTATION } from "./common";
-export interface Settings {
-    displayMode: DISPLAY_MODE;
-    selectionMode: SELECTION_MODE;
-    scrollOrientation: SCROLL_ORIENTATION;
-    firstWeekday: number;
-    minimumDate: Date;
-    maximumDate: Date;
-}
--- node_modules/nativescript-fancy-calendar/src/common.d.ts
+++
@@ -1,5 +0,0 @@
-export * from './calendar-event';
-export * from './calendar-appearance';
-export * from './calendar-enums';
-export * from './calendar-settings';
-export * from './calendar';
